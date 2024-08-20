d4e9513e632ee06e52298d4e05c990739f481359
bugrahankaramollaoglu <bugrahankaramollaoglu@gmail.com>
2024-08-20 09:31:48 +0300

    firebase dependencies added. now will try code part

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -51,9 +52,12 @@ android {
 
 dependencies {
 
-    implementation (libs.accompanist.systemuicontroller)
+    implementation(libs.accompanist.systemuicontroller)
     implementation(libs.cloudy)
     implementation(libs.androidx.ui.text.google.fonts)
+    implementation(platform(libs.firebase.bom))
+
+    implementation(libs.firebase.auth)
 
     /* *********** */
 
