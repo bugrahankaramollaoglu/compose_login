91eee995e2953716fd74397f6d4270c93d932a8a
bugrahankaramollaoglu <bugrahankaramollaoglu@gmail.com>
2024-08-21 11:18:43 +0300

    ok. now will try google sign in

--- app/src/main/java/com/bugrahankaramollaoglu/compose_login/pages/home_page.kt
+++ app/src/main/java/com/bugrahankaramollaoglu/compose_login/pages/home_page.kt
@@ -7,18 +7,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.bugrahankaramollaoglu.compose_login.R
+import com.bugrahankaramollaoglu.compose_login.myT
 import com.bugrahankaramollaoglu.compose_login.utils.signButton
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -29,6 +36,10 @@ val CustomGreen = Color(0xFF75CF9C)
 fun HomePage(navController: NavHostController) {
 
     val mail = Firebase.auth.currentUser?.email
+
+    // State variables for dialog
+    val (showDialog, setShowDialog) = remember { mutableStateOf(false) }
+    val (dialogAction, setDialogAction) = remember { mutableStateOf<(() -> Unit)?>(null) }
 
     val configuration = LocalConfiguration.current
     val screenWidth = configuration.screenWidthDp.dp
@@ -45,8 +56,9 @@ fun HomePage(navController: NavHostController) {
             verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center
         ) {
             if (mail != null) {
-                Text("Welcome,", color = Color.White, fontSize = 30.sp)
-                Text(mail, color = Color.White, fontSize = 30.sp)
+                Text("Welcome,", color = Color.White, fontSize = 30.sp, fontFamily = myT)
+                Spacer(modifier = Modifier.height(screenHeight / 100))
+                Text(mail, color = Color.White, fontSize = 30.sp, fontFamily = myT)
                 Spacer(modifier = Modifier.height(screenHeight / 25))
                 signButton(
                     image = null,
@@ -55,17 +67,73 @@ fun HomePage(navController: NavHostController) {
                     screenHeight = screenHeight / 20,
                     title = "Sign Out"
                 ) {
-                    Firebase.auth.signOut()
+                    setShowDialog(true)
+                    setDialogAction {
+                        Firebase.auth.signOut()
+                        // Optionally, navigate back or update UI
+                    }
                 }
             }
         }
+
+        if (showDialog) {
+            AlertDialog(onDismissRequest = { setShowDialog(false) }, title = {
+                Text(
+                    "Warning", style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = myT,
+                        fontSize = 30.sp,
+                    )
+                )
+            }, text = {
+                Text(
+                    "Are you sure you want to sign out?", style = TextStyle(
+                        fontSize = 17.sp,
+                        fontFamily = myT,
+                    )
+                )
+            }, confirmButton = {
+                Button(
+                    onClick = {
+                        dialogAction?.invoke()
+                        setShowDialog(false)
+                        navController.navigate("main")
+                    }, colors = ButtonDefaults.buttonColors(
+                        containerColor = CustomGreen
+
+                    )
+                ) {
+                    Text(
+                        "Yes", style = TextStyle(
+                            fontFamily = myT,
+                            fontSize = 17.sp,
+                            color = Color.Black.copy(alpha = 0.6f)
+                        )
+                    )
+                }
+            }, dismissButton = {
+                Button(
+                    onClick = { setShowDialog(false) }, colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Black
+
+                    )
+                ) {
+                    Text(
+                        "No", style = TextStyle(
+                            fontFamily = myT,
+                            fontSize = 15.sp,
+                        )
+                    )
+                }
+            })
+        }
+
 
         // Place the Image at the bottom-right corner
         Image(
             painter = painterResource(id = R.drawable.leaff),
             contentDescription = "Leaf",
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
+            modifier = Modifier.align(Alignment.BottomEnd)
         )
     }
 }
